1debe63754154393e8525743838f3a23b67511e8
Gabriel Duarte <142861188+gabrielduo@users.noreply.github.com>
2025-06-08 22:55:56 -0300

    Update app.py

--- src/analise-ambiental/app.py
+++ src/analise-ambiental/app.py
@@ -283,4 +283,6 @@ def report_error():
 
 if __name__ == '__main__':
     # executa a aplicação em modo debug
-    app.run(debug=True)
+    #app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta via variável de ambiente
+    app.run(host='0.0.0.0', port=port, debug=True)
